eeec7b1843ece829a2c4f1728fb3d20cd342e071
adarshgajbhare <adarshgajbhare@gmail.com>
2024-12-31 23:01:39 +0530

    made api key's private

--- src/src/lib/firebase.js
+++ src/src/lib/firebase.js
@@ -4,15 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // Import Firestore
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1F3ct8HYCSr4wBlLM-_6ow_hVMqNw4I4",
-  authDomain: "feed-8aeca.firebaseapp.com",
-  projectId: "feed-8aeca",
-  storageBucket: "feed-8aeca.appspot.com",
-  messagingSenderId: "1001794915344",
-  appId: "1:1001794915344:web:d10fd81eae6cd1c584ce95",
-  measurementId: "G-L1KH2X9MPC"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const auth = getAuth(app);

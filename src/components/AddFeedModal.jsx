9fc7cf8e2d992753042d5655fc45bdc977376cc4
deshmukh-aniruddha <anideshmukh.me@gmail.com>
2025-01-08 02:34:47 +0530

    react toastify added

--- src/components/AddFeedModal.jsx
+++ src/components/AddFeedModal.jsx
@@ -6,6 +6,7 @@ function AddFeedModal({ isOpen, onClose, onAddFeed }) {
   const [feedImage, setFeedImage] = useState(null);
   const [previewImageUrl, setPreviewImageUrl] = useState("/default-thumb.webp");
   const [feedNameError, setFeedNameError] = useState("");
+
   const fileInputRef = useRef(null);
 
   useEffect(() => {
@@ -40,13 +41,6 @@ function AddFeedModal({ isOpen, onClose, onAddFeed }) {
     }
   };
 
-  const handleFeedNameChange = (event) => {
-    setFeedName(event.target.value);
-    if (event.target.value.trim()) {
-      setFeedNameError("");
-    }
-  };
-
   const handleSubmit = async () => {
     if (!feedName.trim()) {
       setFeedNameError("The feed name cannot be empty");
@@ -60,6 +54,13 @@ function AddFeedModal({ isOpen, onClose, onAddFeed }) {
     onClose();
   };
 
+  const handleFeedNameChange = (event) => {
+    setFeedName(event.target.value);
+    if (event.target.value.trim()) {
+      setFeedNameError("");
+    }
+  };
+
   const convertImageToBase64 = (file) => {
     return new Promise((resolve) => {
       const reader = new FileReader();
@@ -70,83 +71,86 @@ function AddFeedModal({ isOpen, onClose, onAddFeed }) {
 
   if (!isOpen) return null;
 
-
   const imageUrl = feedImage
     ? URL.createObjectURL(feedImage)
     : "/default-thumb.webp";
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-black ring-[1px] ring-white/10 p-6 rounded-md w-full max-w-md">
-       <h2 className="text-2xl text-white font-medium tracking-tight mb-6">
-          Create a new feed
-        </h2>
-        <div className="mb-6">
-          <label
-            htmlFor="feedName"
-            className="block text-white mb-3 traccking-tight text-lg/4"
-          >
-            What do you want to call it?
-          </label>
-          <input
-            type="text"
-            id="feedName"
-            className="w-full p-3 text-white placeholder:text-white/35 ring-[1px] ring-white/20 bg-white/5 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Space, Chess, Superheroes, etc."
-            value={feedName}
-            onChange={handleFeedNameChange}
-          />
-          {feedNameError && (
-            <p className="text-red-500 text-sm mt-1">{feedNameError}</p>
-          )}
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="feedImage"
-            className="block mb-3 tracking-tight text-lg/4 text-white"
-          >
-            Set up a cover image
-          </label>
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-black ring-[1px] ring-white/10 p-6 rounded-md w-full max-w-md">
+            <h2 className="text-2xl text-white font-medium tracking-tight mb-6">
+              Create a new feed
+            </h2>
+            <div className="mb-6">
+              <label
+                htmlFor="feedName"
+                className="block text-white mb-3 tracking-tight text-lg/4"
+              >
+                What do you want to call it?
+              </label>
+              <input
+                type="text"
+                id="feedName"
+                className="w-full p-3 text-white placeholder:text-white/35 ring-[1px] ring-white/20 bg-white/5 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Space, Chess, Superheroes, etc."
+                value={feedName}
+                onChange={handleFeedNameChange}
+              />
+              {feedNameError && (
+                <p className="text-red-500 text-sm mt-1">{feedNameError}</p>
+              )}
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="feedImage"
+                className="block mb-3 tracking-tight text-lg/4 text-white"
+              >
+                Set up a cover image
+              </label>
 
-          <div className="flex items-center space-x-4">
-            <img
-              src={previewImageUrl}
-              alt="Feed thumbnail"
-              className="size-10 rounded-md ring-[1px] ring-white/20 object-cover"
-            />
-            <button
-              type="button"
-              onClick={() => fileInputRef.current?.click()}
-              className="grid  place-items-center text-pretty rounded-md text-lg/4 p-3 bg-[#303030] font-medium tracking-tight text-white/90 shadow-[inset_0px_2px_2px_0px_hsla(0,0%,0%,0.4)] drop-shadow-[0px_2px_0px_hsla(0,0%,100%,0.15)]"
-            >
-              Select cover image
-            </button>
-            <input
-              type="file"
-              ref={fileInputRef}
-              accept="image/*"
-              onChange={handleImageChange}
-              className="hidden"
-            />
+              <div className="flex items-center space-x-4">
+                <img
+                  src={previewImageUrl}
+                  alt="Feed thumbnail"
+                  className="size-10 rounded-md ring-[1px] ring-white/20 object-cover"
+                />
+                <button
+                  type="button"
+                  onClick={() => fileInputRef.current?.click()}
+                  className="grid place-items-center text-pretty rounded-md text-lg/4 p-3 bg-[#303030] font-medium tracking-tight text-white/90 shadow-[inset_0px_2px_2px_0px_hsla(0,0%,0%,0.4)] drop-shadow-[0px_2px_0px_hsla(0,0%,100%,0.15)]"
+                >
+                  Select cover image
+                </button>
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="hidden"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4 mt-6">
+              <button
+                className="bg-black ring-[1px] ring-white/20 flex-1 hover:bg-black/50 text-white font-medium tracking-tight text-lg/4 py-3 px-4 rounded-md"
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+              <button
+                className="bg-white hover:bg-white/50 flex-1 text-black font-medium tracking-tight text-lg/4 py-3 px-4 rounded-md"
+                onClick={handleSubmit}
+              >
+                Add
+              </button>
+            </div>
           </div>
         </div>
-
-        <div className="flex items-center gap-4 mt-6">
-          <button
-            className="bg-black ring-[1px] ring-white/20 flex-1 hover:bg-black/50  text-white font-medium tracking-tight text-lg/4 py-3 px-4 rounded-md "
-            onClick={onClose}
-          >
-            Cancel
-          </button>
-          <button
-            className="bg-white hover:bg-white/50  flex-1 text-black font-medium tracking-tight text-lg/4 py-3 px-4 rounded-md"
-            onClick={handleSubmit}
-          >
-            Add
-          </button>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,18 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import AddFeedModal from "../components/AddFeedModal";
 import EditFeedModal from "../components/EditFeedModal";
-import { IconSquareRoundedPlusFilled } from "@tabler/icons-react";
+import {
+  IconCheck,
+  IconCircleCheckFilled,
+  IconSquareCheckFilled,
+  IconSquareRoundedPlusFilled,
+} from "@tabler/icons-react";
 import {
   loadHomeFeeds,
   handleAddHomeFeed,
   handleUpdateHomeFeed,
 } from "../utils/constant";
+import { Icon } from "lucide-react";
 
 function HomePage() {
   const { user } = useAuth();
@@ -16,6 +22,7 @@ function HomePage() {
   const [showEditModal, setShowEditModal] = useState(false);
   const [feeds, setFeeds] = useState([]);
   const [editingFeed, setEditingFeed] = useState(null);
+  const [showToast, setShowToast] = useState(false); // New state for toast
 
   useEffect(() => {
     loadHomeFeeds(user, setFeeds);
@@ -23,6 +30,8 @@ function HomePage() {
 
   const handleAddFeed = async (feedName, imageUrl) => {
     await handleAddHomeFeed(user, feeds, setFeeds, feedName, imageUrl);
+    setShowToast(true); // Show toast after adding feed
+    setTimeout(() => setShowToast(false), 3000); // Hide toast after 3 seconds
   };
 
   const handleUpdateFeed = async (oldName, newName, newImageUrl) => {
@@ -37,9 +46,9 @@ function HomePage() {
   };
 
   return (
-    <div className="w-full min-h-dvh overflow-hidden  rounded-2xl p-0 md:p-6 md:ring-[1px] md:ring-white/20">
+    <div className="w-full min-h-dvh overflow-hidden rounded-2xl p-0 md:p-6 md:ring-[1px] md:ring-white/20">
       <div>
-        <h1 className="text-white text-2xl font-medium  tracking-tight">
+        <h1 className="text-white text-2xl font-medium tracking-tight">
           Hey, {user?.displayName || "there"}
         </h1>
         <p className="text-gray-500 text-sm font-semibold mb-6">
@@ -87,19 +96,42 @@ function HomePage() {
           feed={editingFeed}
         />
       )}
+
+      {/* Toast Notification */}
+      {showToast && (
+        <div className="fixed flex items-start w-80 gap-2.5 top-6 right-6 z-50 bg-[#101010]/50 filter backdrop-blur ring-[1px] ring-white/20 text-white p-4 rounded-lg shadow text-sm font-medium saturate-200">
+           
+          <div className=" flex-shrink-0">
+            <IconSquareCheckFilled
+              size={20}
+              strokeWidth={1}
+              className="relative top-[2px] text-green-500"
+            />
+          </div>
+          <div>
+            <h1 className="text-base font-[600]">
+              New feed created successfully
+            </h1>
+            <p className="text-sm font-[400]  text-gray-400 text-pretty">
+              Your new feed is live. Start adding your favorite channels to
+              personalize it further.
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 function FeedItem({ feed }) {
   return (
-    <div className=" rounded  overflow-hidden transition-all duration-500  ">
+    <div className="rounded overflow-hidden transition-all duration-500">
       <Link to={`/feed/${feed?.name}`} className="block">
         <img
           src={feed?.image || "/placeholder.png"}
           alt={feed?.name}
           className="aspect-video bg-[#272727] 
-          rounded-xl cursor-pointer flex flex-col items-center justify-center gap-2 hover:bg-[#3f3f3f] transition-colors duration-200 "
+          rounded-xl cursor-pointer flex flex-col items-center justify-center gap-2 hover:bg-[#3f3f3f] transition-colors duration-200"
         />
         <div className="my-3">
           <h2 className="text-lg/4 font-medium tracking-tight text-white text-center">

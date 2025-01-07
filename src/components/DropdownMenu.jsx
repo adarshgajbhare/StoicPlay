730237720b8047dfbf2b8476a72ef1afcb33344c
adarshgajbhare <adarshgajbhare@gmail.com>
2025-01-08 01:33:56 +0530

    changes in dropdownmenu

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-
+ 
 const DropdownMenu = ({ 
   isOpen, 
   onClose, 
@@ -8,32 +8,32 @@ const DropdownMenu = ({
   width = 'w-64'
 }) => {
   const dropdownRef = useRef(null);
-
+ 
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
         onClose();
       }
     };
-
+ 
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [onClose]);
-
+ 
   if (!isOpen) return null;
-
+ 
   return (
     <div 
       ref={dropdownRef}
-       className="absolute right-0 top-12 z-20 flex  w-52 flex-col overflow-hidden rounded-lg  
-       bg-[#101010] text-sm/none text-white ring-[1px] ring-white/10"
+       className="absolute right-2 top-12 z-50  flex  w-60 flex-col overflow-hidden rounded-md 
+       bg-black/50 filter backdrop-blur-3xl text-sm/none text-white ring-[1px] ring-white/20"
     >
       {items.map((section, sectionIndex) => (
         <React.Fragment key={sectionIndex}>
-          {sectionIndex > 0 && <div className="h-[1px] bg-red-600" />}
-          <div className="py-1">
+          {/* {sectionIndex > 0 && <div className="h-[1px] bg-red-600" />} */}
+          <div className="">
             {section.map((item, itemIndex) => (
               <button
                 key={itemIndex}
@@ -41,13 +41,13 @@ const DropdownMenu = ({
                   item.onClick();
                   onClose();
                 }}
-                className={`flex w-full items-center justify-between border-b border-white/10  px-2 py-1.5
+                className={`flex w-full items-center justify-between border-b border-white/15 text-lg  px-4 py-2
                   ${item.destructive ? 'text-red-500' : 'text-white'}`}
               >  <span>{item.label}</span>
                 {item.icon && (
                   <span className="size-5">{item.icon}</span>
                 )}
-              
+ 
               </button>
             ))}
           </div>
@@ -56,5 +56,5 @@ const DropdownMenu = ({
     </div>
   );
 };
-
-export default DropdownMenu;
+ 
+export default DropdownMenu

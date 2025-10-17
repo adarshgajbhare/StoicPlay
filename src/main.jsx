f1ed033de262f3c570e0fe68543ba99be2cf408e
Adarsh Gajbhare <107011378+adarshgajbhare@users.noreply.github.com>
2025-10-17 08:59:47 +0530

    Add ToastContainer for notifications in YouTube integration

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,25 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </Provider>
   </React.StrictMode>
 );
